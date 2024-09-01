7cfbeccf7da0e9a278fe718e3d62966fdf5de522
Cole Oliva <olivac2@wwu.edu>
2024-08-31 19:30:22 -0700

    working results display. gonna start button

--- src/components/ResultsDisplay.js
+++ src/components/ResultsDisplay.js
@@ -1,18 +1,31 @@
 import React from "react";
+import dollar from "../images/icon-dollar.svg";
 
 const ResultsDisplay = ({ tipAmount, totalPerPerson }) => {
   return (
     <div className="results">
       <div className="result">
-        <h2>Tip Amount</h2>
-        <p>${tipAmount.toFixed(2)}</p>
+        <div className="results-total">
+          <h2>Tip Amount</h2>
+          <p className="results-para">/ person</p>
+        </div>
+        <span>
+          <img src={dollar} alt="Dollar sign" className="result-dollar" />
+          {tipAmount.toFixed(2)}
+        </span>
       </div>
       <div className="result">
-        <h2>Total</h2>
-        <p>${totalPerPerson.toFixed(2)}</p>
+        <div className="results-total">
+          <h2>Total</h2>
+          <p className="results-para">/ person</p>
+        </div>
+        <span>
+          <img src={dollar} alt="Dollar sign" className="result-dollar" />
+          {totalPerPerson.toFixed(2)}
+        </span>
       </div>
     </div>
   );
-}
+};
 
 export default ResultsDisplay;
